4950fa28a862bc37e00ffa0c534b14bda47d9e11
Jay Nanduri <jay.nanduri@checkmarx.com>
2022-07-26 11:45:06 -0400

    change indentation and sample values

--- Jenkins/jenkinspipeline.groovy
+++ Jenkins/jenkinspipeline.groovy
@@ -4,12 +4,24 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'b1b0d61d-cae1-4757-89bb-5fa680a80731', url: 'https://github.com/jay-nanduri/JayTestZip.git']]])
+                checkout([$class: 'GitSCM',
+                          branches: [[name: '*/main']],
+                          extensions: [],
+                          userRemoteConfigs: [[credentialsId: 'b1b0d61d-cae1-4757-89bb-5fa680a80731', url: 'https://github.com/Checkmarx/ast-cli.git']]])
             }
         }
         stage('Checkmarx Scan') {
             steps {
-                checkmarxASTScanner additionalOptions: '--async', baseAuthUrl: '', branchName: 'main', checkmarxInstallation: 'latest', credentialsId: 'ce75aa2f-9a93-4fe7-98f2-87f9bdc29d93', projectName: 'JayTestZip', serverUrl: 'https://ast-master.dev.cxast.net/', tenantName: 'master_tenant', useOwnAdditionalOptions: true, useOwnServerCredentials: true
+                checkmarxASTScanner additionalOptions: '--async',
+                        baseAuthUrl: '',
+                        branchName: 'main',
+                        checkmarxInstallation: 'latest',
+                        credentialsId: 'ce75aa2f-9a93-4fe7-98f2-87f9bdc29d93',
+                        projectName: 'ast-cli',
+                        serverUrl: 'https://ast-master.dev.cxast.net/',
+                        tenantName: 'master_tenant',
+                        useOwnAdditionalOptions: true,
+                        useOwnServerCredentials: true
             }
         }
     }
