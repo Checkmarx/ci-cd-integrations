8fbf3f9d54fb5815e0059a6dcb715e9aea8bfd90
Igor Lomba <114568996+igorlombacx@users.noreply.github.com>
2022-11-17 10:33:31 +0000

    Example - Getting git branch name from GitSCM in jenkins groovy pipeline (#32)
    * Example - Getting git branch name from GitSCM in jenkins groovy pipeline
    
    * changing server url and tenant name

---
+++ Jenkins/jenkinspipeline-getting-git-branch-from-scm.groovy
@@ -0,0 +1,22 @@
+pipeline {
+    agent any
+    stages {
+        stage('Scan') {
+            steps {
+                script {
+                    def scmVars = checkout([$class: 'GitSCM', branches: [[name: '*/develop']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/WebGoat/WebGoat']]])
+                    checkmarxASTScanner additionalOptions: '--async',
+                            baseAuthUrl: '',
+                            branchName: scmVars.GIT_BRANCH,
+                            checkmarxInstallation: 'latest',
+                            credentialsId: 'ce75aa2f-9a93-4fe7-98f2-87f9bdc29d93',
+                            projectName: 'ast-cli',
+                            serverUrl: 'https://ast.checkmarx.net/',
+                            tenantName: 'my_tenant',
+                            useOwnAdditionalOptions: true,
+                            useOwnServerCredentials: true
+                }
+            }
+        }
+    }
+}
